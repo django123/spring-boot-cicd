fd077faa989e8a26e51ad9c9e512b532a008ad8e
edougajean@gmail.com <edougajean@gmail.com>
2024-11-18 16:21:10 +0100

    my first commit

---
+++ src/test/java/io/django/spring_boot_cicd/SpringBootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package io.django.spring_boot_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
